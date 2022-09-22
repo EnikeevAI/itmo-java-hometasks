2e1b991dba51a60057323f8d9810166ae4acfffd
EnikeevAI <alexander.enikeev@gmail.com>
2022-09-22 16:56:31 +0300

    Task5 comments added

--- src/hw1/Task5.java
+++ src/hw1/Task5.java
@@ -1,9 +1,11 @@
 package hw1;
 
+// Найти сумму цифр 2х значного числа
 public class Task5 {
     public static void main(String[] args) {
         int number = 45;
-        int sumOfNumbers = 45 / 10 + 45 % 10;
+
+        int sumOfNumbers = number / 10 + number % 10;
 
         System.out.println(sumOfNumbers);
 
