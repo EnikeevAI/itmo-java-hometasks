656c6d9de3b20df3f0f251ed4f279c6acde173e3
EnikeevAI <alexander.enikeev@gmail.com>
2022-09-22 13:19:45 +0300

    Task2 added

---
+++ src/hw1/Task2.java
@@ -0,0 +1,9 @@
+package hw1;
+
+// Дано целое число. Найти и вывести в консоль данное число в кубе
+public class Task2 {
+    public static void main(String [] args) {
+        int number = 10;
+        System.out.println(10 * 10 * 10);
+    }
+}
