5afddb5ca6cef65f7e2be1d9a17d00cc191314db
Alexander Enikeev <alexander.enikeev@gmail.com>
2022-09-28 21:35:46 +0300

    Update Task1.java

--- src/hw2/Task1.java
+++ src/hw2/Task1.java
@@ -6,7 +6,7 @@ public class Task1 {
     public static void main(String[] args) {
 
         int month = 11;
-        if (month > 0) {
+        if (month > 0 && month < 13) {
             if (month == 12 || month < 3) {
                 System.out.println("It's winter!");
             } else if (month < 6) {
