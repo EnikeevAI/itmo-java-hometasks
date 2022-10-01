d54c5bd5ca735d4ccedc7c4f7470e47f5f575fcc
EnikeevAI <alexander.enikeev@gmail.com>
2022-10-01 19:07:56 +0300

    hw5 Task2 added

---
+++ src/hw5/Task2.java
@@ -0,0 +1,22 @@
+package hw5;
+
+import java.util.Arrays;
+
+public class Task2 {
+    //Создайте массив из чётных чисел [2;20] и выведите элементы массива в консоль в обратном порядке (20 18 16 ... 4 2)
+    public static void main(String[] args) {
+        int arrLen = 10, arrMin = 2, step = 2;
+        int[] resultArray = new int [10];
+
+        for (int i = 0, arrElem = arrMin; i < resultArray.length; i++, arrElem += step) {
+            resultArray[i] = arrElem;
+        }
+
+        System.out.print("Полученный массив: ");
+        System.out.println(Arrays.toString(resultArray));
+
+        for (int i = resultArray.length -1; i >= 0; i--) {
+            System.out.println(resultArray[i]);
+        }
+    }
+}
