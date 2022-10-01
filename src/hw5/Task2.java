af04cc450a77995e84d042ecd3768812d601a9a7
EnikeevAI <alexander.enikeev@gmail.com>
2022-10-01 19:19:27 +0300

    Изменение в определении длины массива resultArray

--- src/hw5/Task2.java
+++ src/hw5/Task2.java
@@ -6,7 +6,7 @@ public class Task2 {
     //Создайте массив из чётных чисел [2;20] и выведите элементы массива в консоль в обратном порядке (20 18 16 ... 4 2)
     public static void main(String[] args) {
         int arrLen = 10, arrMin = 2, step = 2;
-        int[] resultArray = new int [10];
+        int[] resultArray = new int [arrLen];
 
         for (int i = 0, arrElem = arrMin; i < resultArray.length; i++, arrElem += step) {
             resultArray[i] = arrElem;
