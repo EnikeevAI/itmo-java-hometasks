af94c6d7b542d3f1c5a70419fc0b84abe9a6e0eb
EnikeevAI <alexander.enikeev@gmail.com>
2022-11-13 13:12:09 +0300

    Перенос файлов из урока 21 для выполнения ДЗ

---
+++ src/ru/itmo/lessons/lesson21/TCPServerIO.java
@@ -0,0 +1,44 @@
+package ru.itmo.lessons.lesson21;
+
+import ru.itmo.lessons.lesson21.common.Connection;
+import ru.itmo.lessons.lesson21.common.Message;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServerIO {
+    private int port;
+
+    public TCPServerIO(int port) {
+        this.port = port;
+    }
+
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен...");
+            while (true) {
+                // Связь устанавливается между Socket у клиента и Socket (который создается через метод accept) у сервера
+                Socket socket = serverSocket.accept(); // Пока клиент не подключится дальше инструкции не выполняются
+                // взаимодействие с клиентом (получение и передача информации)
+                Connection<Message> connection = new Connection<>(socket);
+                Message fromClient = connection.readMessage();
+                System.out.println("От клиента: " + fromClient);
+
+                Message message = new Message("Server", "Сообщение от сервера");
+                connection.sendMessage(message);
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Обработка IOException или ClassNotFoundException");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Integer.parseInt(); -> возвращает int
+        // Integer.valueOf(); -> возвращает Integer
+        // что делать, если аргумент не задан или его нельзя преобразовать в int. Или выходит за диапазон портов
+        int port;
+        if (args.length < 1) throw new IllegalArgumentException("Количество аргументов меньше 1");
+        new TCPServerIO(Integer.parseInt(args[0])).run();
+    }
+}
